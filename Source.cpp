ead848ee49742611788fac3faa4125bb7fd936fb
Jonah Bees <j-bees@onu.edu>
2024-04-11 08:09:44 -0400

    DisplayRun ACTUALLY DONE

--- Source.cpp
+++ Source.cpp
@@ -4,61 +4,58 @@
 
 using namespace std;
 
-void generateSequence(int values[], int size) {
-    // Generate 20 random six-sided die tosses
-    srand(time(0));
-    for (int i = 0; i < size; ++i) {
-        values[i] = rand() % 6 + 1; // Generate random number between 1 and 6
-    }
-}
-
 void displayRun(int values[], int size) {
-    bool inRun = false;
-    for (int i = 0; i < size; ++i) {
-        // Check if the current value is the same as the previous one
-        if (i > 0 && values[i] == values[i - 1]) {
-            if (!inRun) {
-                cout << "(" << values[i - 1]; // Start of run
-                inRun = true;
-            }
-            cout << " " << values[i]; // Add value to run
-        }
-        else {
-            if (inRun) {
-                cout << ")"; // End of run
-                inRun = false;
-            }
-            cout << " " << values[i]; // Print value
-        }
-    }
-    if (inRun) {
-        cout << ")"; // End of run for the last run
-    }
-    cout << endl;
-}
+    int i;
+    int inRun = 0; // Flag to indicate if we're currently in a run
 
-bool hasRun(int values[], int size) {
-    for (int i = 1; i < size; ++i) {
-        // Check if the current value is the same as the previous one
-        if (values[i] == values[i - 1]) {
-            return true; // Found a run
+    for (i = 0; i < size; i++) {
+        // Check if the current value is the same as the next value
+        if (i < size - 1 && values[i] == values[i + 1]) {
+            // If not already in a run, start the run
+            if (!inRun) {
+                inRun = 1;
+                cout << "(" << values[i] << " ";
+            }
+            // If already in a run, just print the value without space
+            else {
+                cout << values[i] << " ";
+            }
+        }
+        // If not in a run and the current value is different from the next one
+        else if (!inRun) {
+            cout << values[i] << " ";
+        }
+        // If we're in a run and the current value is different from the next one
+        else {
+            inRun = 0;
+            cout << values[i] << ") ";
         }
     }
-    return false; // No run found
 }
 
 int main() {
-    const int size = 20;
-    int values[size];
+    int i;
+    int dieValues[20];
 
-    bool foundRun = false;
-    while (!foundRun) {
-        generateSequence(values, size);
-        foundRun = hasRun(values, size);
+    // Seed the random number generator
+    srand(time(NULL));
+
+    // Generate 20 random six-sided die tosses
+    for (i = 0; i < 20; i++) {
+        dieValues[i] = rand() % 6 + 1;
     }
 
-    cout << "Sequence with a run:";
-    displayRun(values, size);
+    // Print the die values
+    cout << "Die Values: ";
+    for (i = 0; i < 20; i++) {
+        cout << dieValues[i] << " ";
+    }
+    cout << endl;
+
+    // Print the die values with runs
+    cout << "With Runs: ";
+    displayRun(dieValues, 20);
+    cout << endl;
 
     return 0;
 }
